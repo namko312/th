b5aef1e6b56452060580d5b225eb97d2759e38fd
duc <nguyen>
2019-03-10 15:24:03 +0700

    ad

--- api/controllers/AdController.js
+++ api/controllers/AdController.js
@@ -11,12 +11,12 @@ module.exports = {
     try {
       res.json({
         success: true,
-        ad: {
-          title: 'Quang cao ShopeeLike',
-          description: 'Quang cao ShopeeLike',
-          image: 'https://helpx.adobe.com/nz/stock/how-to/visual-reverse-image-search/_jcr_content/main-pars/image.img.jpg/visual-reverse-image-search-v2_1000x560.jpg',
-          url: 'https://facebook.com/shopeelike'
-        }
+        // ad: {
+        //   title: 'Quang cao ShopeeLike',
+        //   description: 'Quang cao ShopeeLike',
+        //   image: 'https://helpx.adobe.com/nz/stock/how-to/visual-reverse-image-search/_jcr_content/main-pars/image.img.jpg/visual-reverse-image-search-v2_1000x560.jpg',
+        //   url: 'https://facebook.com/shopeelike'
+        // }
       })
     } catch (err) {
       res.json({
